6956471785ec40d0e026637feb1d609f456c323a
Priscila_Borges <priscila.borges.nl@gmail.com>
2026-07-27 18:32:39 +0200

    parsing done

---
+++ a_maze_ing.py
@@ -0,0 +1,3 @@
+from parser import parse_config
+
+print(parse_config("config.txt"))
---
+++ parser.py
@@ -0,0 +1,96 @@
+from dataclasses import dataclass
+
+@dataclass
+class Config:
+	width: int
+	height: int
+	maze_entry: tuple[int, int]
+	maze_exit: tuple[int, int]
+	output_file: str
+	perfect: bool
+	seed: int
+
+def read_config_file(read_file_path: str) -> dict[str, str]:
+	raw_data: dict[str, str] = {}
+
+	with open(read_file_path, "r") as file:
+		for line_num, line in enumerate(file, start=1):
+			line = line.strip()
+
+			if not line or line.startswith("#"):
+				continue
+			
+			if "=" not in line:
+				raise ValueError(f"Syntax error on line {line_num}: missing '='")
+
+			key, value = line.split("=", 1)
+			raw_data[key.strip()] = value.strip()
+
+	return raw_data
+
+def parse_config(file_path: str) -> Config:
+	raw_data = read_config_file(file_path)
+
+	required_keys = {
+		"WIDTH", "HEIGHT", "ENTRY", "EXIT", "OUTPUT_FILE", "PERFECT"
+	}
+
+	missing_keys = required_keys - raw_data.keys()
+	if missing_keys:
+		raise ValueError(f"Missing mandatory keys")
+
+	try:
+		width = int(raw_data["WIDTH"])
+		height = int(raw_data["HEIGHT"])
+	except ValueError:
+		raise ValueError("WIDTH and HEIGHT must be valid integers.")
+
+	if width <= 0 or height <= 0:
+		raise ValueError("WIDTH and HEIGHT must be greater than 0.")
+
+	m_entry = raw_data["ENTRY"]
+	m_exit = raw_data["EXIT"]
+
+	try:
+		x_entry, y_entry = m_entry.split(",")
+		x_exit, y_exit = m_exit.split(",")
+		
+	except ValueError:
+		raise ValueError("ENTRY and EXIT must be formated as x, y")
+
+	try:
+		maze_entry: tuple[int, int] = (int(x_entry), int(y_entry))
+		maze_exit: tuple[int, int] =  (int(x_exit), int(y_exit))
+	except ValueError:
+		raise ValueError("ENTRY and EXIT must be a positive integer")
+	
+	perfect = raw_data["PERFECT"]
+	if not perfect == "True" or perfect == "False":
+		raise ValueError("Perfect must be either TRUE or FALSE")
+
+	try:
+		perfect = bool(perfect)
+	except ValueError:
+		raise ValueError("Need to be bool")
+
+	try:
+		seed = int(raw_data["SEED"])
+	except ValueError:
+		raise ValueError("SEED must be a positive integer")
+	
+	output_file = raw_data["OUTPUT_FILE"]
+	if not output_file:
+		raise ValueError("OUTPUT_FILE cannot be empty")
+
+	try:
+		output_file = str(output_file)
+	except ValueError:
+		raise ValueError("OUTPUT_FILE must be a string")
+
+	return (width,
+			height,
+			m_entry,
+			m_exit,
+			output_file,
+			perfect,
+			seed)
