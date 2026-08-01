0321f5a7227c5fbfab2b45cbd9d4c9b14e6abb3d
Priscila_Borges <priscila.borges.nl@gmail.com>
2026-08-01 16:41:35 +0200

    adjusted maze class and created constructor class to print 2d array

--- a_maze_ing.py
+++ a_maze_ing.py
@@ -1,10 +1,13 @@
 from parser import parse_config
-from maze import Maze
+from maze import Maze, Constructor
 
 def main():
 	config_data = parse_config("config.txt")
-	test1 = Maze(config_data.width, config_data.height)
-	print(test1.gen_maze())
+	test_maze = Maze(config_data.width, config_data.height)
+	builder = Constructor(test_maze, config_data.maze_entry[0], config_data.maze_entry[1])
+	builder.generate()
+	
+	test_maze.print_matrix()
 
 
 if __name__ == "__main__":
--- maze.py
+++ maze.py
@@ -1,16 +1,58 @@
 import random
 
 class Maze:
-	def __init__(self, height, width) -> None:
+	def __init__(self, width: int, height: int) -> None:
+		self.width = width
 		self.height = height
-		self.width = width	
-	
-	def gen_maze(self) -> list[[list[int]]]:
-		self.maze = []
+		self.maze: dict[tuple[int, int], int] = {}
+
 		for x in range(self.width):
-			self.row = []
 			for y in range(self.height):
-				self.row.append(x)
-			self.maze.append(self.row)
-		return self.maze
+				self.maze[(x, y)] = 15
+
+	def print_matrix(self) -> None:
+		for y in range(self.height):
+			row = [self.maze[(x, y)] for x in range(self.width)]
+			print(row)
+	
+
+class Constructor:
+	def __init__(self, maze: Maze, start_x: int, start_y: int) -> None:
+		self.maze = maze
+		self.start_x = start_x
+		self.start_y = start_y
+		self.visited: set[tuple[int, int]] = {(start_x, start_y)}
+		self.stack: list[tuple[int, int]] = [(start_x, start_y)]
+		self.directions = {
+			'N': (0, -1, 1, 4),
+			'E': (1, 0, 2, 8),
+			'S': (0, 1, 4, 1),
+			'W': (-1, 0, 8, 2),
+		}
+
+
+	#DFS algorithm to open the walls and create the maze
+	def generate(self) -> None:
+		while self.stack:
+			curr_x, curr_y = self.stack[-1]
+			neighbors = []
+
+			for name, (dir_x, dir_y, curr_bit, neighbor_bit) in self.directions.items():
+				neighbor_x, neighbor_y = curr_x + dir_x, curr_y + dir_y
+
+				if 0 <= neighbor_x < self.maze.width and 0 <= neighbor_y < self.maze.height:
+					if (neighbor_x, neighbor_y) not in self.visited:
+						neighbors.append((neighbor_x, neighbor_y, curr_bit, neighbor_bit))
+
+			if neighbors:
+				neighbor_x, neighbor_y, curr_bit, neighbor_bit = random.choice(neighbors)
+
+				self.maze.maze[(curr_x, curr_y)] &= ~curr_bit
+				self.maze.maze[(neighbor_x, neighbor_y)] &= ~neighbor_bit
+
+				self.visited.add((neighbor_x, neighbor_y))
+				self.stack.append((neighbor_x, neighbor_y))
+
+			else:
+				self.stack.pop()
 
