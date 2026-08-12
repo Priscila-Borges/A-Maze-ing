233410d93733917b7ace3d6dde1368a367a255cb
Priscila_Borges <priscila.borges.nl@gmail.com>
2026-08-12 18:24:42 +0200

    implemented pac-man maze rules for imperfect maze

--- mazegen/generator.py
+++ mazegen/generator.py
@@ -48,19 +48,65 @@ class MazeGenerator:
                 self.stack.pop()
 
     def make_imperfect(self) -> None:
+        self._open_key_locations()
+        self._create_loops()
+        self._remove_dead_ends()
+
+    def _open_key_locations(self) -> None:
+        center_x = self.maze.width // 2
+        center_y = self.maze.height // 2
+
+        key_cells = [
+            (0, 0), #top-left
+            (self.maze.width -1, 0), #top-right
+            (0, self.maze.height -1), #bottom-left
+            (self.maze.width -1, self.maze.height -1), #bottom-right
+            (center_x, center_y), #center
+        ]
+
+        for x, y in key_cells:
+            self._open_random_wall(x, y)
+
+    def _create_loops(self) -> None:
+        """Randomly knocks down interior walls to create loops."""
+        for x in range(self.maze.width - 1):
+            for y in range(self.maze.height -1):
+                if random.random() < 0.15:
+                    self._open_random_wall(x, y)
+
+    def _remove_dead_ends(self) -> None:
+        """Finds cells with 3 walls intact and opens one side."""
         for x in range(self.maze.width):
             for y in range(self.maze.height):
-                if random.random() < 0.10:
-                    direction = random.choice(["E", "S"])
-                    dir_x, dir_y, curr_bit, n_bit = self.ways[direction]
+                if self._count_walls(x, y) == 3:
+                    self._open_random_wall(x, y)
 
-                    neighbor_x = x + dir_x
-                    neighbor_y = y + dir_y
+    def _count_walls(self, x: int, y: int) -> int:
+        """Counts how many of the 4 walls are closed for a cell."""
+        cell_value = self.maze.cells[(x, y)]
+        walls = 0
 
-                    if (
-                        0 <= neighbor_x < self.maze.width
-                        and 0 <= neighbor_y < self.maze.height
-                    ):
+        if cell_value & 1:
+            walls += 1
+        if cell_value & 2:
+            walls += 1
+        if cell_value & 4:
+            walls += 1
+        if cell_value & 8:
+            walls += 1
 
-                        self.maze.cells[(x, y)] &= ~curr_bit
-                        self.maze.cells[(neighbor_x, neighbor_y)] &= ~n_bit
+        return walls
+
+    def _open_random_wall(self, x: int, y: int) -> None:
+        """Attempts to break a wall between cell (x, y) and a valid neighbor."""
+        directions = list(self.ways.keys())
+        random.shuffle(directions)
+
+        for direction in directions:
+            dx, dy, curr_bit, n_bit = self.ways[direction]
+            nx, ny = x + dx, y + dy
+
+            if 0 <= nx < self.maze.width and 0 <= ny < self.maze.height:
+                self.maze.cells[(x, y)] &= ~curr_bit
+                self.maze.cells[(nx, ny)] &= ~n_bit
+                break    
